2f9a8ae261596783b6a1b71ec7d72ed3cc25d138
Narendra G <gorantla04@gmail.com>
2020-07-28 20:25:13 +0530

    Initial commit with basic spring boot rest api

--- src/main/java/com/naren/springsecurity/demo/security/SecurityConfiguration.java
+++ src/main/java/com/naren/springsecurity/demo/security/SecurityConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
