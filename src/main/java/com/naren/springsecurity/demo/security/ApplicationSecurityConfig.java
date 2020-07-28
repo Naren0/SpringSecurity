f7841fa9590b864c11e04e34d1f4697a08731f43
Narendra G <gorantla04@gmail.com>
2020-07-28 19:26:47 +0530

    Initial commit with basic spring boot rest api

---
+++ src/main/java/com/naren/springsecurity/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.naren.springsecurity.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
---
+++ src/main/java/com/naren/springsecurity/demo/student/StudentController.java
@@ -0,0 +1,39 @@
+package com.naren.springsecurity.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1,"Naren"),
+            new Student(2,"Anil"),
+            new Student(3,"Ravi")
+    );
+
+    @GetMapping(path ="/{studentId}")
+    public Student getStudent(@PathVariable Integer studentId){
+            return  STUDENTS.stream()
+                    .filter(student -> studentId.equals(student.getStudentId()))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("Student "+studentId+" not found in list"));
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "Student and Admin role";
+    }
+
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "Admin role only";
+    }
+}
